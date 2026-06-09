44cf64c06d93857e0bd0c4a240e3010d1c07bdd4
Maria Florencia Quintana <101899984+Shahiro91@users.noreply.github.com>
2026-06-09 12:11:02 -0300

    WIP: guardar cambios admin antes de merge

--- Alumno/views.py
+++ Alumno/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
-from .models import Alumno
+from Alumno.models import Alumno
+from Clase.models import Clase
+from Plan.models import Plan
+from Profesor.models import Profesor
+from Reclamos.models import Reclamos
 
 # Create your views here.
 
 def lista_alumnos(request):
     alumnos = Alumno.objects.all()
     return render(request, 'listaAlumnos.html', {'alumnos': alumnos})
+
+
+def admin_panel(request):
+    alumnos = Alumno.objects.all()
+    clases = Clase.objects.all()
+    profesores = Profesor.objects.all()
+    planes = Plan.objects.all()
+    reclamos = Reclamos.objects.all()
+
+    context = {
+        'alumnos': alumnos,
+        'clases': clases,
+        'profesores': profesores,
+        'planes': planes,
+        'reclamos': reclamos,
+    }
+
+    return render(request, 'admin.html', context)
