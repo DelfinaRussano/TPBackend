90e7f4180fbb2fe167f6fdcfff47ea662601b760
Maria Florencia Quintana <101899984+Shahiro91@users.noreply.github.com>
2026-06-19 21:13:26 -0300

    Creación del panel del admin y creación de las migraciones para las tablas de la base de datos.

--- tpBackend/urls.py
+++ tpBackend/urls.py
@@ -20,6 +20,9 @@ from Alumno import views as alumno_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('clases/', include('Clase.urls')),
+    path('profesores/', include('Profesor.urls')),
+    path('planes/', include('Plan.urls')),
     path('alumnos/', include('Alumno.urls')),
     path('mi-admin/', alumno_views.admin_panel, name='admin_panel'),
 ]
