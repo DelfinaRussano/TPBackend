76e7dc0157ca5131c7d95123ce011a6198bbc2ee
DelfinaRussano <115591908+DelfinaRussano@users.noreply.github.com>
2026-06-21 19:03:41 -0300

    Fix merge conflict in Profesor/views.py
    Removed merge conflict markers and cleaned up imports.

--- Profesor/views.py
+++ Profesor/views.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.shortcuts import render, get_object_or_404
 from .models import Profesor
 from Clase.models import Clase
@@ -49,7 +48,6 @@ def lista_profesores(request):
         'profesores': profesores,
     })
 
-=======
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Profesor
@@ -75,4 +73,4 @@ def crear_profesor(request):
 
 	clases = Clase.objects.all()
 	return render(request, 'crear_profesor.html', {'clases': clases})
->>>>>>> origin/main
+
